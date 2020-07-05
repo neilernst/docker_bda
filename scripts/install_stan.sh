a9f69cfd994afe7aa446c43c1d00b8a6aa263886
Richard Torkar <richard.torkar@gmail.com>
2020-07-05 13:58:43 +0200

    Update install_stan.sh

--- scripts/install_stan.sh
+++ scripts/install_stan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-apt-get -y install apt-utils libnode-dev libxt6 \
+apt-get -y install apt-utils libnode-dev libxt6 libxml2-dev \
 && install2.r --error --skipinstalled \
     brms \
     rstanarm \
