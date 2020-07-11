e08b6684829735e9f347691cb3204ececfd94d88
Richard Torkar <richard.torkar@gmail.com>
2020-07-11 15:46:49 +0200

    Update install_stan.sh

--- scripts/install_stan.sh
+++ scripts/install_stan.sh
@@ -3,7 +3,6 @@ set -e
 
 apt-get -y install apt-utils libnode-dev libxt6 libxml2-dev libglpk-dev\
 && install2.r --error --skipinstalled \
-    brms \
     rstanarm \
     tidybayes \
     blavaan \
@@ -26,4 +25,5 @@ apt-get -y install apt-utils libnode-dev libxt6 libxml2-dev libglpk-dev\
     tufte \
     openxlsx \
 && installGithub.r rmcelreath/rethinking \
+&& installGithub.r paul-buerkner/brms \
 && rm -rf /tmp/downloaded_packages/ /tmp/*.rds
